33ce19c56c8cdf1d89f2d3405af2ebd0dfc7e929
desperado <youtubhexo@gmail.com>
2024-07-31 10:55:09 +0800

    图形验证码功能开发

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,67 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <div class="layui-container">
+    <form class="layui-form layui-form-pane" action="">
+   <div class="layui-form-item">
+    <label class="layui-form-label">用户名</label>
+    <div class="layui-input-block">
+      <input type="text" name="title" autocomplete="off" placeholder="请输入" lay-verify="required" class="layui-input">
+    </div>
+  </div>
+  <div class="layui-form-item">
+    <label class="layui-form-label">密码</label>
+    <div class="layui-input-block">
+      <input type="text" name="title" autocomplete="off" placeholder="请输入" lay-verify="required" class="layui-input">
+    </div>
+  </div>
+  <div class="layui-form-item">
+    <label class="layui-form-label">验证码</label>
+    <div class="layui-input-inline">
+      <input  type="text" name="title" autocomplete="off" placeholder="请输入" lay-verify="required" class="layui-input">
+    </div>
+    <div class="layui-form-mid layui-text-em svg" v-html="svg" @click="getCaptcha"></div>
+  </div>
+  <button type="button" class="layui-btn">点击登录</button>
+  <a class="desperado-link" href="https://www.desperado.cloud/">忘记密码</a>
+  </form>
+</div>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import axios from 'axios'
+export default {
+  name: 'App',
+  data () {
+    return {
+      svg: ''
+    }
+  },
+  mounted () {
+    this.getCaptcha()
+  },
+  methods: {
+    getCaptcha () {
+      axios.get('http://localhost:3000/captcha').then((res) => {
+        this.svg = res.data
+      })
     }
   }
+
+}
+
+</script>
+<style lang="scss"  scoped>
+.desperado-link {
+  margin-left: 10px;
+  &:hover {
+    color: #16baaa;
+  }
+}
+input {
+  width: 190px;
+}
+.svg{
+  position: relative;
+  top: -15px
 }
 </style>
